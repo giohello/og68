83ff019616377964864984f82befeef530650b7d
giohello <gio2009bero@gmail.com>
2025-04-24 16:32:28 +0400

    hi

--- routes/blogs.js
+++ routes/blogs.js
@@ -10,7 +10,33 @@ const requireAuth = (req, res, next) => {
 }
 
 router.get('/', requireAuth, function (req, res, next) {
-    res.render('blogs', {error: null});
+    const filePath = path.join(__dirname, "..", "blogs.json");
+    const data = fs.readFileSync(filePath);
+    const blogs = JSON.parse(data);
+    res.render("blogs", { blogs });
 });
+
+const fs = require("fs");
+const path = require("path");
+
+router.post("/", requireAuth, (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = {
+        title,
+        content,
+        author: req.session.user.email,
+        date: new Date().toLocaleString()
+    };
+
+    const filePath = path.join(__dirname, "..", "blogs.json");
+    const data = fs.readFileSync(filePath);
+    const blogs = JSON.parse(data);
+    blogs.push(blog);
+    fs.writeFileSync(filePath, JSON.stringify(blogs, null, 2));
+
+    res.redirect("/blogs");
+});
+
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,27 +1,45 @@
 const express = require('express');
-const {request, json} = require("express");
-const fs = require("fs")
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
-const USERS_FILE = "users.json"
+const USERS_FILE = "users.json";
 
 router.get('/', function (req, res, next) {
-    res.render('register', {error: null});
+    res.render('register', { error: null });
 });
 
 router.post('/', function (req, res, next) {
-    const {email, password, confirmPassword} = req.body
+    const { email, password, confirmPassword } = req.body;
+
     if (password !== confirmPassword) {
-        res.render('register', {error: "Passwords don't match"});
+        return res.render('register', { error: "Passwords don't match" });
     }
 
-    const data = fs.readFileSync(USERS_FILE)
-    const users = JSON.parse(data)
-    users.push({email, password});
-    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2))
+    let data;
+    try {
+        data = fs.readFileSync(USERS_FILE);
+    } catch (err) {
+        data = "[]";
+    }
 
-    res.render('register', {error: null});
+    let users;
+    try {
+        users = JSON.parse(data);
+    } catch (err) {
+        users = [];
+    }
+
+    if (users.find(user => user.email === email)) {
+        return res.render('register', { error: "Email already exists" });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    users.push({ email, password: hashedPassword });
+
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+
+    res.redirect('/login');
 });
-
 
 module.exports = router;
